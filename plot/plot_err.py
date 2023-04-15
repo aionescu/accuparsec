6082c5bd714fc94a6e840516d89481a622913b7c
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2023-04-15 19:33:39 +0200

    Added plotting script for errors

--- plot/plot_err.py
+++ plot/plot_err.py
@@ -1,13 +1,24 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
+accu = [123, 123, 123]
+atto = [123, 12, 131]
+
+lengths = ["24", "43", "1111"]
+width = 1 / len(accu)
+ 
 (fig, ax) = plt.subplots(layout="constrained", figsize=(6.4, 4.8), dpi=150)
-library_count = len(benchmarks)
-width = 1 / (library_count+1)
-# for (library_index, (library, data)) in enumerate(sorted(benchmarks.items(), key=itemgetter(0))):
-#     x = np.arange(len(data))
-#     ax.bar(x + library_index * width, data.values(), width, label=library)
-ax.set_ylabel("input size")
-ax.set_xlabel("avg distance from error", usetex=x_label_usetex)
-ax.set_xticks(x + (library_count-1) * width / 2 , tick_labels, usetex=xtick_usetex, rotation=rotation)
-# ax.legend(loc="upper left", ncol=legend_column_count if legend_column_count is not None else library_count)
-fig.savefig("bench_speed_json_slow.png")
+
+indices = np.arange(len(lengths))
+
+ax.bar(indices - width/2, atto, width, label='Attoparsec')
+ax.bar(indices + width/2, accu, width, label='Accuparsec')
+
+ax.set_xticks(indices)
+ax.set_xticklabels(lengths)
+
+ax.set_ylabel("Average distance from error")
+ax.set_xlabel("Input size", usetex=False)
+
+fig.savefig("err.png")
+# fig.show()
